d49aef6549c51544bd739f5b1207fa51e970a58d
Vitali Varabuey <72112909+Vorobey1992@users.noreply.github.com>
2023-06-21 16:55:36 +0300

    changed realization Load and Save mothods

--- Reflection/Task1/ConfigurationComponentBase.cs
+++ Reflection/Task1/ConfigurationComponentBase.cs
@@ -19,85 +19,6 @@ namespace Reflection.Task1
             providerTypes = GetAvailableProviderTypes();
         }
 
-        /*public void SaveSettings()
-        {
-            try
-            {
-                var properties = typeof(ProgramSettings).GetProperties();
-                foreach (var property in properties)
-                {
-                    if (Attribute.GetCustomAttribute(property, typeof(ConfigurationItemAttribute)) is ConfigurationItemAttribute attribute)
-                    {
-                        var value = property.GetValue(Settings);
-
-                        if (value != null && value.GetType().IsGenericType && value.GetType().GetGenericTypeDefinition() == typeof(GenericSetting<>))
-                        {
-                            // Получение значения свойства Value
-                            var genericSettingValue = value.GetType().GetProperty("Value")?.GetValue(value);
-
-                            // Создание экземпляра провайдера
-                            if (Activator.CreateInstance(attribute.ProviderType) is IConfigurationProvider provider)
-                            {
-                                Type providerType = attribute.ProviderType;
-                                MethodInfo? setSettingMethod = providerType.GetMethod("SetSetting");
-                                MethodInfo? genericSetSettingMethod = setSettingMethod.MakeGenericMethod(genericSettingValue.GetType());
-
-                                // Вызов метода SetSetting<T> с передачей значения
-                                genericSetSettingMethod?.Invoke(provider, new object?[] { attribute.SettingName, genericSettingValue });
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error when saving settings: " + ex.Message);
-            }
-        }
-
-
-        public void LoadSettings()
-        {
-            try
-            {
-                var properties = typeof(ProgramSettings).GetProperties();
-
-                foreach (var property in properties)
-                {
-                    if (Attribute.GetCustomAttribute(property, typeof(ConfigurationItemAttribute)) is ConfigurationItemAttribute attribute)
-                    {
-                        if (Activator.CreateInstance(attribute.ProviderType) is IConfigurationProvider provider)
-                        {
-                            Type providerType = attribute.ProviderType;
-                            MethodInfo? getSettingMethod = providerType.GetMethod("GetSetting");
-
-                            // Получаем обобщенный аргумент типа T
-                            Type genericArgumentType = property.PropertyType.GetGenericArguments()[0];
-
-                            // Создание обобщенного метода GetSetting<T>
-                            MethodInfo? genericGetSettingMethod = getSettingMethod.MakeGenericMethod(genericArgumentType);
-
-                            // Создание делегата для вызова метода GetSetting<T>
-                            var getSettingDelegate = (Func<string, object>)Delegate.CreateDelegate(typeof(Func<string, object>), provider, genericGetSettingMethod);
-
-                            // Вызов метода GetSetting<T> с указанием типа T
-                            dynamic value = getSettingDelegate.Invoke(attribute.SettingName);
-
-                            // Создание экземпляра GenericSetting<T> и установка значения свойства Value
-                            var genericSetting = Activator.CreateInstance(typeof(GenericSetting<>).MakeGenericType(genericArgumentType));
-                            genericSetting?.GetType().GetProperty("Value")?.SetValue(genericSetting, value.Value);
-
-                            // Установка значения свойства объекта ProgramSettings
-                            property.SetValue(Settings, genericSetting);
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error when loading settings: " + ex.Message);
-            }
-        }*/
         public void SaveSettings()
         {
             try
@@ -117,12 +38,16 @@ namespace Reflection.Task1
                             {
                                 foreach (var providerType in providerTypes)
                                 {
-                                    var provider = Activator.CreateInstance(providerType) as IConfigurationProvider;
+                                    if (attribute.ProviderType.FullName == providerType.FullName)
+                                    {
 
-                                    MethodInfo? setSettingMethod = providerType.GetMethod("SetSetting");
-                                    MethodInfo? genericSetSettingMethod = setSettingMethod.MakeGenericMethod(genericSettingValue.GetType());
+                                        var provider = Activator.CreateInstance(providerType) as IConfigurationProvider;
 
-                                    genericSetSettingMethod?.Invoke(provider, new object[] { attribute.SettingName, genericSettingValue });
+                                        MethodInfo? setSettingMethod = providerType.GetMethod("SetSetting");
+                                        MethodInfo? genericSetSettingMethod = setSettingMethod.MakeGenericMethod(genericSettingValue.GetType());
+
+                                        genericSetSettingMethod?.Invoke(provider, new object[] { attribute.SettingName, genericSettingValue });
+                                    }
                                 }
                             }
                         }
@@ -153,30 +78,31 @@ namespace Reflection.Task1
                         {
                             var provider = Activator.CreateInstance(providerType) as IConfigurationProvider;
 
-                            MethodInfo? getSettingMethod = providerType.GetMethod("GetSetting");
-
-                            MethodInfo? genericGetSettingMethod = getSettingMethod.MakeGenericMethod(genericArgumentType);
-
-                            var getSettingDelegate = Delegate.CreateDelegate(typeof(Func<string, object>), provider, genericGetSettingMethod) as Func<string, object>;
-
-                            dynamic value = getSettingDelegate.Invoke(attribute.SettingName);
-
-                            if (value != null)
+                            if (attribute.ProviderType.FullName == providerType.FullName)
                             {
-                                var genericSetting = Activator.CreateInstance(typeof(GenericSetting<>).MakeGenericType(genericArgumentType), attribute.SettingName);
-                                genericSetting?.GetType().GetProperty("Value")?.SetValue(genericSetting, value.Value);
-                                property.SetValue(Settings, genericSetting);
-                            }
-                            else
-                            {
-                                // Значение по умолчанию, если строка настроек не найдена
-                                var defaultValue = ""; 
-                                var genericSetting = Activator.CreateInstance(typeof(GenericSetting<>).MakeGenericType(genericArgumentType), attribute.SettingName);
-                                genericSetting?.GetType().GetProperty("Value")?.SetValue(genericSetting, defaultValue);
-                                property.SetValue(Settings, genericSetting);
-                            }
+                                MethodInfo? getSettingMethod = providerType.GetMethod("GetSetting");
 
-                            
+                                MethodInfo? genericGetSettingMethod = getSettingMethod.MakeGenericMethod(genericArgumentType);
+
+                                var getSettingDelegate = Delegate.CreateDelegate(typeof(Func<string, object>), provider, genericGetSettingMethod) as Func<string, object>;
+
+                                dynamic value = getSettingDelegate.Invoke(attribute.SettingName);
+
+                                if (value != null)
+                                {
+                                    var genericSetting = Activator.CreateInstance(typeof(GenericSetting<>).MakeGenericType(genericArgumentType), attribute.SettingName);
+                                    genericSetting?.GetType().GetProperty("Value")?.SetValue(genericSetting, value.Value);
+                                    property.SetValue(Settings, genericSetting);
+                                }
+                                else
+                                {
+                                    // Значение по умолчанию, если строка настроек не найдена
+                                    var defaultValue = "";
+                                    var genericSetting = Activator.CreateInstance(typeof(GenericSetting<>).MakeGenericType(genericArgumentType), attribute.SettingName);
+                                    genericSetting?.GetType().GetProperty("Value")?.SetValue(genericSetting, defaultValue);
+                                    property.SetValue(Settings, genericSetting);
+                                }
+                            }
                         }
                     }
                 }
@@ -219,5 +145,3 @@ namespace Reflection.Task1
 
     }
 }
-
-
